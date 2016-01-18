2d50839e4764d2ed8aca6fc4040c7ee6b714f7e9
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2016-01-18 12:00:08 +0100

    Add cs_CZ locale

---
+++ lang/cs/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'User Plus+',
+        'description' => 'Přidává uživatelům profilová políčka.',
+    ],
+    'tab' => [
+        'profile' => 'Profil',
+    ],
+    'user' => [
+        'phone' => 'Telefon',
+        'mobile' => 'Mobil',
+        'company' => 'Firma',
+        'city' => 'Město',
+        'zip' => 'PSČ',
+        'street_addr' => 'Ulice',
+    ],
+];
