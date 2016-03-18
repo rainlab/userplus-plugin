b3ced1f50fced15825b187d2f935705602063b39
Jean Marc BRUNO <technique@edps.fr>
2016-03-18 12:41:06 +0100

    Add French Locale

---
+++ lang/fr/lang.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'plugin' => [
+        'name' => 'User Plus+',
+        'description' => 'Ajoute des champs de profil aux utilisateurs.',
+    ],
+    'user' => [
+        'phone' => 'Téléhone',
+        'mobile' => 'Mobile',
+        'company' => 'Société',
+        'city' => 'Ville',
+        'zip' => 'Code postal',
+        'street_addr' => 'Addresse',
+    ],
+];
