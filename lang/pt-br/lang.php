e938257362daa43273640d2c15fa867e5c813475
Ronaldo Ribeiro de Sousa <ribbsousa@gmail.com>
2017-02-03 09:45:23 -0300

    Add lang pt-br
    Add lang pt-br

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Usuário Plus+',
+        'description' => 'Adiciona campos de perfil aos usuários.',
+    ],
+    'tab' => [
+        'profile' => 'Perfil',
+    ],
+    'user' => [
+        'phone' => 'Telefone',
+        'mobile' => 'Celular',
+        'company' => 'Empresa',
+        'city' => 'Cidade',
+        'zip' => 'Cep',
+        'street_addr' => 'Endereço',
+    ],
+];
