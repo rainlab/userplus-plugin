e0a1760222bb60796aadf13d4931e186f2eee350
Szabó Gergő <gergo85@gmail.com>
2017-07-27 10:00:19 +0200

    Add the Hungarian translate

---
+++ lang/hu/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Felhasználók Plusz+',
+        'description' => 'Új személyes mezők a felhasználók adatlapjához.',
+    ],
+    'tab' => [
+        'profile' => 'Személyes',
+    ],
+    'user' => [
+        'phone' => 'Telefon',
+        'mobile' => 'Mobil',
+        'company' => 'Cég',
+        'city' => 'Város',
+        'zip' => 'Irszám.',
+        'street_addr' => 'Postacím',
+    ],
+];
