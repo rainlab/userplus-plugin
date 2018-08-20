d760e09fda969ce74d26fe4ee32e422db497976e
Nathan van der Werf <nvanderwerf93@gmail.com>
2018-08-20 21:16:49 +0200

    Add NL translation

---
+++ lang/nl/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Gebruiker Plus+',
+        'description' => 'Voegt profielvelden toe aan gebruikers.',
+    ],
+    'tab' => [
+        'profile' => 'Profiel',
+    ],
+    'user' => [
+        'phone' => 'Telefoon',
+        'mobile' => 'Mobiel',
+        'company' => 'Bedrijf',
+        'city' => 'Stad',
+        'zip' => 'Postcode',
+        'street_addr' => 'Adres',
+    ],
+];
