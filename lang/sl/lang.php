5d7c22140584a54c77f6f83b797aac407bd795ed
Blaž Oražem <blaz@orazem.si>
2020-09-17 06:14:08 +0200

    Slovenian language added (#33)

---
+++ lang/sl/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Uporabniki Plus+',
+        'description' => 'Razširitev vtičnika Uporabniki z dodatnimi polji za profil.',
+    ],
+    'tab' => [
+        'profile' => 'Profil',
+    ],
+    'user' => [
+        'phone' => 'Telefon',
+        'mobile' => 'Mobilni',
+        'company' => 'Podjetje',
+        'city' => 'Mesto',
+        'zip' => 'Pošta',
+        'street_addr' => 'Naslov',
+    ],
+];
