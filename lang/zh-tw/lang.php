2ba915a93fb6a77cd7be11c8cbb364b9f7d9e268
lctoan <i7851@hotmail.com>
2020-09-17 12:13:01 +0800

    Create Taiwanese translation file (#37)
    * create zh-tw translation file
    
    * create zh-tw translation file

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => '會員+',
+        'description' => '增加會員資料欄位。',
+    ],
+    'tab' => [
+        'profile' => '會員資料',
+    ],
+    'user' => [
+        'phone' => '電話',
+        'mobile' => '手機',
+        'company' => '公司名稱',
+        'city' => '城市',
+        'zip' => '郵遞區號',
+        'street_addr' => '地址',
+    ],
+];
